eed584eb214fb6267c07cb49c407b8d95fa43940
Romain Francois <romain@rstudio.com>
2020-02-16 11:56:40 +0100

    Initial commit

---
+++ R/lay.R
@@ -0,0 +1,55 @@
+#' lay down
+#'
+#' @param .data A tibble
+#' @param fn A function to apply to each row of `.data`. May also be a formula, see [rlang::as_function()].
+#'
+#' @details
+#' - each row of `.data` is materialized into a vector via [vctrs::vec_c()]
+#' - `fn` is applied to that vector
+#' - All results are finally combined together with [vctrs::vec_c()]
+#'
+#' @importFrom rlang as_function
+#' @importFrom vctrs vec_c
+#' @importFrom purrr pmap
+#'
+#' @examples
+#' if (require("dplyr")) {
+#'
+#'   # for printing
+#'   iris <- as_tibble(iris)
+#'
+#'   # apply mean for each row
+#'   iris %>%
+#'     mutate(sepal = lay(across(starts_with("Sepal")), mean))
+#'
+#'   # not that useful because there is `rowMeans` already
+#'   iris %>%
+#'     mutate(sepal = rowMeans(across(starts_with("Sepal"))))
+#'
+#'   # but then we can lay other functions, e.g. median
+#'   iris %>%
+#'     mutate(sepal = lay(across(starts_with("Sepal")), median))
+#'
+#'   # or lay into a tibble if you want multiple results
+#'   iris %>%
+#'     mutate(sepal = lay(
+#'       across(starts_with("Sepal")),
+#'       ~tibble(min = min(.), mean = mean(.), max = max(.))
+#'     ))
+#'
+#'   # and if you skip `sepal =` things get auto spliced for you
+#'   iris %>%
+#'     mutate(lay(
+#'       across(starts_with("Sepal")),
+#'       ~tibble(min = min(.), mean = mean(.), max = max(.))
+#'     ))
+#' }
+#'
+#' @export
+lay <- function(.data, fn) {
+  fn <- as_function(fn)
+  bits <- pmap(.data, function(...) {
+    fn(vec_c(...))
+  })
+  vec_c(!!!bits)
+}
