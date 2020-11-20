64e32ab5b6b6701a0d8969ed5f0497f48f3e2c2b
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-11-20 17:39:48 +0100

    More tests

--- tests/testthat/test-lay.R
+++ tests/testthat/test-lay.R
@@ -1,6 +1,11 @@
 test_that("lay works", {
-  df <- tibble(x = 1:10, y = 11:20, z = 21:30)
 
+  ## data for tests
+  df <- tibble(x = 1:10, y = 11:20, z = 21:30)
+  df_na <- df
+  df_na[1, 1] <- NA
+
+  ## simple calls
   expect_identical(
     lay(df, mean),
     rowMeans(df)
@@ -11,9 +16,37 @@ test_that("lay works", {
     df$x
   )
 
+  ## call with fn arguments
   expect_identical(
-    lay(df, ~tibble(min = min(.), max = max(.))),
+    lay(df_na, mean, na.rm = TRUE),
+    rowMeans(df_na, na.rm = TRUE)
+  )
+
+  ## auto spliced output
+  expect_identical(
+    lay(df, ~ tibble(min = min(.x), max = max(.x))),
     tibble(min = df$x, max = df$z)
+  )
+
+  ## both methods should lead to same results
+  expect_identical(
+    lay(df, mean, .method = "tidy"),
+    lay(df, mean, .method = "apply")
+  )
+
+  expect_identical(
+    lay(df, ~ mean(.x), .method = "tidy"),
+    lay(df, ~ mean(.x), .method = "apply")
+  )
+
+  expect_identical(
+    lay(df, ~ tibble(min = min(.x), max = max(.x)), method = "tidy"),
+    lay(df, ~ tibble(min = min(.x), max = max(.x)), method = "apply")
+  )
+
+  ## handle error properly
+  expect_error(
+    lay(df, mean, .method = "nonesense")
   )
 
 })
