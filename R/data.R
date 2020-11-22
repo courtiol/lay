7a59706e0f56bad541ce899fda1cb3bcc9a4554b
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-11-22 14:55:52 +0100

    Smaller data subset for examples

--- R/data.R
+++ R/data.R
@@ -1,13 +1,14 @@
 #' Pain relievers misuse in the US
 #'
-#' A dataset containing information about the use of pain relievers for non medical purpose.
+#' Datasets containing information about the use of pain relievers for non medical purpose.
 #'
-#' This dataset is a small subset from the "National Survey on Drug Use and Health, 2014".
+#' These datasets are a small subset from the "National Survey on Drug Use and Health, 2014".
 #' All variables related to drug use have been recoded into vectors of integers talking value 0 for
 #' "No/Unknown" and value 1 for "Yes". The original variable names were the same as those defined
-#' here but in upper case and ending with the number 2.
+#' here but in upper case and ending with the number 2. The dataset called `drugs` contain the first
+#' 100 rows of the one called `drugs_full`.
 #'
-#' @format A tibble with 55271 rows and 9 variables:
+#' @format A tibble with either 100 or 55271 rows, and 9 variables:
 #' \describe{
 #'   \item{caseid}{The identifier code of the respondent}
 #'   \item{hydrocd}{Ever use hydrocodone nonmedically?}
@@ -26,6 +27,10 @@
 #'     National Survey on Drug Use and Health, 2014.
 #'     Ann Arbor, MI: Inter-university Consortium for Political and Social Research (distributor), 2016-03-22.
 #'     \url{https://doi.org/10.3886/ICPSR36361.v1}
+#'
+#' @aliases drugs drugs_full
+#' @name drugs
 #' @examples
 #' drugs
-"drugs"
+#' drugs_full
+NULL
